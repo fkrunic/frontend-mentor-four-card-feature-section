b3e234f9c2270b078badccb0f99085005b572fa4
Filip Krunic <fkrunic9@gmail.com>
2023-09-05 16:25:36 -0700

    project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Four Card Feature Section
+Implementation of [Four Card Feature Section](https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK/hub) using Vue and TailwindCSS.
---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <p class="text-3xl underline font-bold">
+      Hello world!
+    </p>
+  </div>
+</template>
