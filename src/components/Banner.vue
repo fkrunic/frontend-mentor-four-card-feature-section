e164a08262d1936ac79e44218fe06c6a7aaae194
Filip Krunic <fkrunic9@gmail.com>
2023-09-05 17:09:04 -0700

    banner component

---
+++ src/components/Banner.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <p>Reliable, efficient delivery</p>
+    <p class="font-bold">Powered by Technology</p>
+    <p>
+      Our Artifical Intelligence powered tools 
+      use millions of project data points to ensure 
+      that your project is successful
+    </p>
+  </div>
+</template>
