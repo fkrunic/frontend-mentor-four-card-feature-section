ff3740a2a4d93312dc7d0a3cb15d54a50e1c741b
Filip Krunic <fkrunic9@gmail.com>
2023-09-05 17:07:37 -0700

    basic card component

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  title: string,
+  content: string, 
+  iconURL: string,
+  borderColor: string
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col items-start shadow-lg">
+    <p>{{ title }}</p>
+    <p>{{ content }}</p>
+    <div class="flex flex-row justify-end items-center">
+      <img :src="iconURL">
+    </div>
+  </div>
+</template>
+
